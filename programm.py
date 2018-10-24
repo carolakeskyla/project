60da70ea3b721d14ef4e968280fa0f4b7d790cc0
Carola Kesküla <carolakeskyla@gmail.com>
2018-10-24 13:30:31 +0300

    Adding file, new fun()

---
+++ programm.py
@@ -0,0 +1,17 @@
+#sys - võtame vastu süsteemi poolt antud sisendit
+#argv - sisendilist nt. [sisendArv1, sisendArv2]
+from sys import argv
+
+#väärtustame sisendid (py <script> a b)
+script, a, b = argv
+
+#sisendina saame String tüüpi väärtused 
+a = int(a)
+b = int(b)
+
+#defineerime lihtsa liitmisfunktsiooni 
+def fun(a, b):
+	print(a+b)
+
+#kutsume funktsiooni välja 
+fun(a, b)
